f537e63c9b428a57c5b77b8128b25e2d5a2937f0
Cauã Gonçalves <gonssalvescaua@gmail.com>
2024-06-04 11:06:59 -0300

    Criado esquelo do projeto Banco

---
+++ br/banco/classes/Cadastro.java
@@ -0,0 +1,51 @@
+package br.banco.classes;
+
+public class Cadastro {
+
+	private String nomeDoCliente;
+	private int numeroDaConta;
+	private double saldo;
+	
+	
+	public Cadastro(String nomeDoCliente, int numeroDaConta) {
+		super();
+		this.nomeDoCliente = nomeDoCliente;
+		this.numeroDaConta = numeroDaConta;
+	}
+
+	public Cadastro(String nomeDoCliente, int numeroDaConta, double depositoInicial) {
+		this.nomeDoCliente = nomeDoCliente;
+		this.numeroDaConta = numeroDaConta;
+		deposita(depositoInicial);
+	}
+	
+	public String toString() {
+		return " - Nome: "+ nomeDoCliente +
+		" - Conta: "+ numeroDaConta +
+		" - Saldo atual: $ "+ saldo;
+	}
+
+	public void deposita (double valorDoDeposito) {
+		 saldo += valorDoDeposito;
+	}
+
+	public void saca (double valorDoSaque) {
+		 saldo -= valorDoSaque + 5;
+	}
+
+	public String getNomeDoCliente() {
+		return nomeDoCliente;
+	}
+
+	public void setNomeDoCliente(String nomeDoCliente) {
+		this.nomeDoCliente = nomeDoCliente;
+	}
+
+	public int getNumeroDaConta() {
+		return numeroDaConta;
+	}
+	
+	public double getSaldo() {
+		return saldo;
+	}
+}
---
+++ br/banco/principal/Clientes.java
@@ -0,0 +1,52 @@
+package br.banco.principal;
+
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
+import br.banco.classes.Cadastro;
+
+public class Clientes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Cadastro conta;
+
+		System.out.println("Olá, vamos criar sua conta!");
+		System.out.print("Digite o numero da sua conta: ");
+		int numero = sc.nextInt();
+		System.out.print("Digite o seu nome: ");
+		sc.nextLine();
+		String nome = sc.nextLine();
+		System.out.print("Quer fazer o deposito inicial?(y/n): ");
+		char resposta = sc.next().charAt(0);
+		if (resposta == 'y') {
+			System.out.println("Qual o valor do deposito?: ");
+			double depositoInicial = sc.nextInt();
+			conta = new Cadastro(nome, numero, depositoInicial);
+		} 
+		else {
+			conta = new Cadastro(nome, numero);
+		}
+		
+		System.out.println();
+		System.out.println("Os dados da sua conta são:");
+		System.out.println(conta);
+		
+		System.out.println();
+		System.out.print("Digite um valor para deposito: ");
+		double valorDeposito = sc.nextDouble();
+		conta.deposita(valorDeposito);
+		System.err.println();
+		System.out.println("Conta com o novo saldo: ");
+		System.err.println(conta);
+		
+		System.out.println();
+		System.out.print("Digite um valor para saque: ");
+		double valorSaque = sc.nextDouble();
+		conta.saca(valorSaque);
+		System.err.println();
+		System.out.println("Conta com o novo saldo: ");
+		System.err.println(conta);
+		
+		sc.close();
+	}
+}
